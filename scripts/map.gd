3ab522eea86755b79aed8342d7b0c039d0149d39
Eric Rodefeld <eric.rodefeld@gmail.com>
2024-08-29 09:06:55 -0400

    Start port work

--- scripts/map.gd
+++ scripts/map.gd
@@ -3,19 +3,21 @@ extends Node2D
 class_name WorldMap
 
 @export var noise_height_texture: NoiseTexture2D
+@export var tile_data: Dictionary = {}
 
 var noise: Noise
 var tile_map_layers: Array[StyxLayer] = []
-
 func _ready() -> void:
 	noise = noise_height_texture.noise
 	var _tile_map_layers = $tile_map_layers.get_children()
 	for _tile_map_layer in _tile_map_layers:
 		tile_map_layers.append(_tile_map_layer)
+		tile_data[_tile_map_layer.name] = _tile_map_layer
+
 	
 func generate(width, height) -> void:
-	for x in range(-width/ 2 , width/2):
-		for y in range(-height/ 2 , height/2):
+	for x in range(0 , width):
+		for y in range(0, height):
 			var noise_val: float = noise.get_noise_2d(x, y)
 			var vector_pos = Vector2i(x, y)
 			for tile_map_layer in tile_map_layers:
@@ -35,16 +37,5 @@ func generate(width, height) -> void:
 		else:
 			for vector_pos in tile_map_layer.tile_positions:
 				tile_map_layer.set_cell(vector_pos, 0, tile_map_layer.atlas_coords.pick_random())
-	#
-	#for vector_pos in water_0_tiles:
-		#water_0_layer.set_cell(vector_pos, source_id, water_atlas_coords)
-	#beach_0_layer.set_cells_terrain_connect(beach_0_tiles, 3, 0)
-	#ground_0_layer.set_cells_terrain_connect(ground_0_tiles, 2, 0)
-	#ground_1_layer.set_cells_terrain_connect(ground_1_tiles, 1, 0)
-	#for vector_pos in ground_2_tiles:
-		#ground_2_layer.set_cell(vector_pos, source_id, grass_atlas.pick_random())
-	#cliff_0_layer.set_cells_terrain_connect(cliff_0_tiles, 4, 0)
-	#for vector_pos in building_0_tiles:
-		#ground_2_layer.set_cell(vector_pos, source_id, building_atlas_coords)
-	#
-		
+
+	
--- scripts/world.gd
+++ scripts/world.gd
@@ -1,9 +1,14 @@
 extends Node2D
 
+const port_scene: PackedScene = preload("res://scenes/port.tscn")
 
 
 @onready var map = $map
 @onready var camera = $Player/Camera2D
+
+
+@export var ports = []
+
 
 
 var width: int = 100
@@ -11,10 +16,24 @@ var height: int = 100
 
 func _ready():
 	map.generate(width, height)
+	generate_ports()
+	
 	
 func _input(_event):
 	if Input.is_action_just_pressed("zoom_in"):
 		camera.zoom *= 2	
 	elif Input.is_action_just_pressed("zoom_out"):
 		camera.zoom *= .5
+
+
+func generate_ports():
+	var port_coords: Array[Vector2i] = map.tile_data['building_water_0'].tile_positions
+	for port_coord in port_coords:
+		var port = port_scene.instantiate()
+		port_coord.x *= 16
+		port_coord.y *= 16
+		port.global_position = port_coord
+		self.add_child(port)
+	
+	
 	
